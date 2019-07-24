f858cfa92eb6885015147acb0403c83cd4ca1a56
Shraddha Kulkarni <shraddha@carvingit.com>
2019-07-24 16:03:09 +0530

    Add code for file upload

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::view('/upload',"upload");
+Route::post('upload_resume','UploadResume@index');
