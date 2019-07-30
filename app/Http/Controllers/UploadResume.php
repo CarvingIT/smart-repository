24abe9137318963802427e31baff254ea0f13511
Shraddha Kulkarni <shraddha@carvingit.com>
2019-07-30 17:03:50 +0530

    Resume content gets inserted in database

--- app/Http/Controllers/UploadResume.php
+++ app/Http/Controllers/UploadResume.php
@@ -4,15 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\File;
+
+use Illuminate\Support\Facades\Storage;
+#use Illuminate\Support\Facades\Input;
+#use Illuminate\Support\Facades\DB;
+
 class UploadResume extends Controller
 {
     //
-	function index(Request $request){
+	public function index()
+    	{
+       		return view('upload');
+   	}
+
+	public function uploadResume(Request $request){
                 #echo "Upload Resume in DOCX format";
-                $success = $request->file('resume')->store('uploaded_resumes');
+		if($request->hasFile('resume')){
+			$filename = $request->file('resume')->getClientOriginalName();
+			$filepath = $request->file('resume')->storeAs('uploaded_resumes',$filename);
+			$filesize = $request->file('resume')->getClientSize();
+			$mimetype = $request->file('resume')->getMimeType();
+			$success = $request->file('resume')->storeAs('uploaded_resumes',$filename);
+       		        #$success = $request->file('resume')->store('uploaded_resumes');
+
+			$file = new File;
+			$file->name = $filename;
+			$file->size = $filesize;
+			$file->mime = $mimetype;
+			$data = file_get_contents($request->file('resume')->getRealPath());
+			$file->content = $data;
+			echo $data;
+			$file->save();
+		}
                 if(!empty($success)){
-                echo "Uploaded resume successfully!<br />";
-                echo "<a href='/upload'>Click here to upload another document</a>";
+                	echo "Uploaded resume successfully!<br />";
+                	echo "<a href='/upload'>Click here to upload another document</a>";
                 }
         }
 
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::view('/upload',"upload");
-Route::post('upload_resume','UploadResume@index');
+Route::get('/upload', 'UploadResume@index')->name('upload');
+#Route::view('/upload',"upload");
+Route::post('upload_resume','UploadResume@UploadResume');
