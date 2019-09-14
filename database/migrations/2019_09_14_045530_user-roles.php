1961fcc07f6766eb7355de246ea8fe0c47288565
Ketan Kulkarni <ketan@carvingit.com>
2019-09-14 11:22:13 +0530

    database migrations to support user-roles added

---
+++ database/migrations/2019_09_14_045530_user-roles.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserRoles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function(Blueprint $table) {
+            $table->increments('id');
+            $table->char('name', 100);
+        });
+
+        Schema::create('user_roles', function(Blueprint $table){
+            $table->increments('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+        });
+
+        //add foreign keys
+        Schema::table('user_roles', function(Blueprint $table){
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('roles');
+    }
+}
