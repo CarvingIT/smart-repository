c60d3011b598f049814a75bc06677b317cd3d001
Shraddha Kulkarni <shraddha@carvingit.com>
2019-09-18 16:13:25 +0530

    Upload resume

--- app/Http/Controllers/UploadResume.php
+++ app/Http/Controllers/UploadResume.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\File;
+use App\Classes\DocxToTextConversion;
+
+
 
 use Illuminate\Support\Facades\Storage;
 #use Illuminate\Support\Facades\Input;
@@ -22,19 +25,21 @@ class UploadResume extends Controller
                 #echo "Upload Resume in DOCX format";
 		if($request->hasFile('resume')){
 			$filename = $request->file('resume')->getClientOriginalName();
-			$filepath = $request->file('resume')->storeAs('uploaded_resumes',$filename);
+			$filepath = $request->file('resume')->storeAs('public/uploaded_resumes',$filename);
 			$filesize = $request->file('resume')->getClientSize();
 			$mimetype = $request->file('resume')->getMimeType();
-			$success = $request->file('resume')->storeAs('uploaded_resumes',$filename);
-       		        #$success = $request->file('resume')->store('uploaded_resumes');
+			$success = $request->file('resume')->storeAs('public/uploaded_resumes',$filename);
+			echo $filepath;
+			$converter = new DocxToTextConversion('/public/uploaded_resumes/'.$filename);
+			$data = $converter->convertToText();
+			echo $data;
+exit;
 
 			$file = new File;
 			$file->name = $filename;
 			$file->size = $filesize;
 			$file->mime = $mimetype;
-			$data = file_get_contents($request->file('resume')->getRealPath());
 			$file->content = $data;
-			echo $data;
 			$file->save();
 		}
                 if(!empty($success)){
