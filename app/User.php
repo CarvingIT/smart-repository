6e5e0edffc054b0bc69679a86f50597b4236a2f5
Shraddha Kulkarni <shraddha@carvingit.com>
2019-12-04 11:46:56 +0530

    page design changes

--- app/User.php
+++ app/User.php
@@ -37,6 +37,9 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+	'created_at' => 'datetime',
+	'updated_at' => 'datetime',
+	'deleted_at' => 'datetime'
     ];
 
     public function roles(){
--- resources/views/collectionmanagement.blade.php
+++ resources/views/collectionmanagement.blade.php
@@ -8,14 +8,20 @@ $(document).ready(function() {
 } );
 </script>
 <div class="container" style="margin-top:5%;">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
             <div class="card">
+            <div class="card-header card-header-primary">
                 <div class="card-header">Collections
-                    <div class="card-header-corner"><a href="/admin/collection-form/new"><img class="icon" src="/i/plus.png" style="width:4%;"/></a></div>
+                    <!--div class="card-header-corner"><a href="/admin/collection-form/new"><img class="icon" src="/i/plus.png" style="width:4%;"/></a></div-->
                 </div>
+              </div>		
 
                 <div class="card-body">
+		<div class="row">
+                    <div class="col-12 text-right"><a href="/admin/collection-form/new" class="btn btn-sm btn-primary">Add Collection</a></div>
+		</div>
                     <div class="flash-message">
                     @foreach (['danger', 'warning', 'success', 'info'] as $msg)
                         @if(Session::has('alert-' . $msg))
@@ -23,14 +29,14 @@ $(document).ready(function() {
                         @endif
                     @endforeach
                     </div>
-
-                    <table id="collections" class="display" style="width:100%">
-                        <thead>
+			<div class="table-responsive">
+                    <table id="collections" class="table">
+                        <thead class="text-primary">
                             <tr>
                             <th>Name</th>
                             <th>Type</th>
                             <th>Created</th>
-                            <th>Actions</th>
+                            <th class="text-right">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -39,17 +45,19 @@ $(document).ready(function() {
                             <td>{{ $c->name }}</td>
                             <td>{{ $c->type }}</td>
                             <td>{{ $c->created_at }}</td>
-                            <td>
-                                <a href="/admin/collection-form/{{$c->id}}"><img class="icon" src="/i/pencil-edit-button.png" style="width:13%;/></a>
-                                <a href="/admin/collection-form/{{$c->id}}/delete"><img class="icon" src="/i/trash.png" style="width:13%;" /></a>
+                            <td class="text-right">
+                                <a rel="tooltip" class="btn btn-success btn-link" href="/admin/collection-form/{{$c->id}}"><img class="material-icon" src="/i/pencil-edit-button.png" width=20% /></a>
+                                <a rel="tooltip" class="btn btn-success btn-link" href="/admin/collection-form/{{$c->id}}/delete"><img class="material-icon" src="/i/trash.png" style="width:20%;" /></a>
                             </td>    <!-- use font awesome icons or image icons -->
                         </tr>
                         @endforeach
                         </tbody>
                     </table>
+			</div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
