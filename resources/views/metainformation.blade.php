39401c8c83d8aee8b160fabb483674359db295bd
Shraddha Kulkarni <shraddha@carvingit.com>
2020-05-08 07:23:33 +0530

    code fixed in metainformation blade to hide the Options field when Text field is selected as to add meta info

--- resources/views/metainformation.blade.php
+++ resources/views/metainformation.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+<script type="text/javascript">
 $( document ).ready(function() {
     $("#options-field").hide();
 
