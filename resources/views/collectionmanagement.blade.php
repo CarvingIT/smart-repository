2cdd1af4471fff59b1ff904ca444cfb08bcd2636
Shraddha Kulkarni <shraddha@carvingit.com>
2020-05-15 13:47:57 +0530

    addded only jquery.3.3.1.js in the head section and rest of the structure of app blade kept as it was previously

--- resources/views/collectionmanagement.blade.php
+++ resources/views/collectionmanagement.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
 <script>
 $(document).ready(function() {
