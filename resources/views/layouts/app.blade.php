0bef68e8d476f4d1c6c7c87aeeff0e483d2fc619
Ketan Kulkarni <ketan@carvingit.com>
2020-08-11 14:44:46 +0530

    minor updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ __('Smart Repository - Preserve Your Important Documents.') }}</title>
+    <title>{{ __('Smart Repository - Knowledge management made easy') }}</title>
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('material') }}/img/apple-icon.png">
     <link rel="icon" type="image/png" href="{{ asset('material') }}/img/favicon.png">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- CSS Files -->
     <link href="{{ asset('material') }}/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
+    <link href="/css/custom.css" rel="stylesheet" />
     <!-- CSS Just for demo purpose, don't include it in your project -->
     <!--link href="{{ asset('material') }}/demo/demo.css" rel="stylesheet" /-->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
