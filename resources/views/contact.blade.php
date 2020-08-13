274b4ee17730fac1addda0bad2ad5b923bc15658
Ketan Kulkarni <ketan@carvingit.com>
2020-08-13 23:14:18 +0530

    minor updates

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,8 +11,8 @@
                 <div class="card-body">
                     <p>If you would like to request a demo, have any questions or suggest a feature, we shall be glad to hear from you!</p>
                     <p>
-                    Email: <a href="mailto:info@smartpeople.com">info@smartpeople.com</a><br/>
-                    Phone: +91 86057 89833
+                    Email: <a href="mailto:info@carvingit.com">info@carvingit.com</a><br/>
+                    Phone: +91 94201 21704, +91 86057 89833
                     </p>
                 </div>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,15 @@
 <div class="row justify-content-center">
       <div class="col-md-12">
 <div class="card">
-<div class="card-header card-header-primary"><h4 class="card-title">Welcome To Smart Repository</h4></div>
+<div class="card-header card-header-primary"><h4 class="card-title">Smart Repository</h4></div>
 <div class="card-body">
-	<p><strong>Document management</strong> is very Easy Now with <strong>Smart Repository</strong>. Until now your organization stores, manages and tracks its electronic documents on google drive or some other external resources. It incorporates document, document repositories, multiple revisions and search systems. Now we are presenting all these facilities in one software. It has some more unique features...</p> 
-	<strong>Smart Search</strong> You have definitely heard of Smartphones; have you heard of <strong>Smart Search</strong>?
+    <img style="float:left;" src="/i/wordcloud.png" />
+    <h3>Welcome to Smart Repository!</h3>
+	<p><strong>Knowledge management</strong> is very Easy Now with <strong>Smart Repository</strong>. Organizations store, manage and track their electronic documents on cloud drives or some other external resources. Now we are presenting all these facilities in one software. It has some more unique features...</p> 
+	<strong>Smart Search</strong>: You have definitely heard of Smartphones; have you heard of <strong>Smart Search</strong>?
 	<p>Presenting one of its kind Smart Search functionality that enables you to search any text content from your multiple files that is uploaded on the software. The good news is that it doesn’t just search the word in the title but in the entire file as well to generate accurate and suitable Search result.</p>
 	<p><strong>What is OCR?</strong> This Smart Search feature uses <strong>OCR (Optical Character Recognition)</strong> at the backend that allows you to get accurate English text content from the images uploaded. For example, if you upload screenshot of your chat on whatsapp, the Search feature allows you to search the text content on the image uploaded also. Isn’t that great? No doubt, this is a one shot solution to your Searching requirements.</p>
-	<p><strong>Revisions</strong> Our software not only searches the document but also keeps its multiple versions also.</p>
-<!--/div-->
+    <p>For a full list of features, click <a href="/features">here</a>.</p>
 </div>
 </div>
       </div>
