d810af4d9253641082fecdcfd2f8819ccdd5d60e
Ketan Kulkarni <ketan@carvingit.com>
2020-09-22 19:02:31 +0530

    spidering appears to be working for content-type=text/html

--- app/CrawlHandler.php
+++ app/CrawlHandler.php
@@ -4,26 +4,69 @@ use Spatie\Crawler\CrawlObserver;
 use GuzzleHttp\Exception\RequestException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\UriInterface;
+use App\Url;
 
 class CrawlHandler extends CrawlObserver{
 
    public function willCrawl(UriInterface $url) {
-	echo "Starting crawling $url\n";
+	//echo "Starting crawling $url\n";
    }
 
    public function crawled(UriInterface $url, ResponseInterface $response, ?UriInterface $foundOnUrl = null){
-	echo "Crawled $url\n";
+	//echo "Crawled $url\n";
+	$status_code = $response->getStatusCode();
+		if($status_code == '200'){
+			$body = $response->getBody();
+			$body->seek(0);
+			$content = '';
+			while($c = $body->read(1024)){
+				$content .= $c;
+			}
+			$headers = $response->getHeaders();
+			$url_model = new Url;
+			$url_model->collection_id = 1; // this should be dynamic
+			$url_model->url = (string)$url;
+			$url_model->type = $headers['Content-Type'][0];
+			$url_model->text_content = $this->getText($headers['Content-Type'][0], $content);
+			$url_model->title = $this->getTitle($headers['Content-Type'][0], $content);
+			$url_model->save();
+		}
    }
 
-   public function crawlFailed(
-        UriInterface $url,
-        RequestException $requestException,
-        ?UriInterface $foundOnUrl = null
-   ){
+   public function crawlFailed(UriInterface $url, RequestException $requestException, ?UriInterface $foundOnUrl = null){
 	 echo "Failed crawling $url\n";
    }
 
-    public function finishedCrawling() {
-	echo "Finished crawling\n";
-    }
+   public function finishedCrawling() {
+	  echo "Finished crawling\n";
+   }
+
+   private function getTitle($mime_type, $content){
+	if(preg_match('/text\/html/', $mime_type)){
+		$title = preg_match('/<title>(.*?)<\/title>/s', $content, $matches);
+		return $matches[1];
+	}
+	else{
+		return ''; // code needs to be handled for mime-types other than text/html
+	}
+   }
+
+   private function getText($mime_type, $content){
+	if(preg_match('/text\/html/', $mime_type)){
+		$html = new \Html2Text\Html2Text($content);
+		return $html->getText();
+	}
+	else{
+		return ''; // code needs to be handled for mime-types other than text/html
+	}
+	/*
+	$tmp_path = sys_get_temp_dir().'/'.\Uuid::generate()->string.'.'.$ext;
+	$fh = fopen($tmp_path,'w+');
+	fwrite($fh, $content);
+	fclose($fh);
+	$doc = new \App\DocXtract($tmp_path);
+       	$text = $doc->convertToText();
+	unlink($tmp_path);
+	*/
+   }
 }
---
+++ database/migrations/2020_09_22_015507_urls.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Urls extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('collection_id')->unsigned();
+            $table->string('title')->nullable();
+            $table->string('url');
+            $table->string('type');
+            $table->longText('text_content')->nullable();
+	    $table->binary('browsershot')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        //add foreign keys
+        Schema::table('urls', function(Blueprint $table){
+            $table->foreign('collection_id')->references('id')->on('collections');
+        });
+
+       // Full Text Index
+       DB::statement('ALTER TABLE documents ADD FULLTEXT fulltext_index_urls (title, text_content)');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
