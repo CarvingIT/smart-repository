0d1c3082105ebf80d56cefddbe6b5e84c52a723e
Ketan Kulkarni <ketan@carvingit.com>
2021-01-11 16:48:13 +0530

    cards are smaller now on wide devices

--- resources/views/collections.blade.php
+++ resources/views/collections.blade.php
@@ -4,22 +4,22 @@
 <div class="container">
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            @foreach ($collections as $c)
+        @foreach ($collections as $c)
+        <div class="col-sm-12 col-md-4">
             <div class="card">
             <div class="card-header card-header-primary">
                   @if ($c->type == 'Members Only')
                     <i class="material-icons">lock</i>
                   @endif
-                <span class="card-title"><a href="/collection/{{ $c->id }}" title="Click here to view documents">{{ $c->name }}</a> 
+                <span class="card-title"><a href="/collection/{{ $c->id }}" title="Click here to view documents">{{ $c->name }}</a> ({{ count($c->documents) }}) 
             </span>
             </div>
                   <div class="card-body">
                   {{ $c->description }}
                  </div>
             </div>
-          @endforeach
         </div>
+        @endforeach
     </div>
 </div>
 </div>
