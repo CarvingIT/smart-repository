d9276029445a4f63607d7369ca80a1a0bc2c6375
Ketan Kulkarni <ketan@carvingit.com>
2021-03-23 19:02:09 +0530

    Minor blade update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,10 +13,12 @@
 <div class="row justify-content-center">
       <div class="col-md-12">
 		<div class="card">
-			<div class="card-header card-header-primary"><h4 class="card-title">PUNE NAGAR VACHAN MANDIR</h4></div>
+			<div class="card-header card-header-primary"><h4 class="card-title">{{ env('APP_NAME') }}</h4></div>
 			<div class="card-body">
 				<div class="row justify-content-center">
+				@if(!empty($settings['banner_image_1']))
     			<img class="img-responsive" src="{{ $settings['banner_image_1'] }}" />
+				@endif
 				</div>
 				{!! $settings['home_page'] !!}
 			</div>
