73ccdee460d46688961b112e115a1c24c933db80
Ketan Kulkarni <ketan@carvingit.com>
2021-05-03 19:45:04 +0530

    Added command to copy contents of a disk to another

---
+++ app/Console/Commands/DiskCopy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class DiskCopy extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'SR:DiskCopy {source_disk : Source Disk} {destination_disk : Destination Disk}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Copies all files from one disk to another';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+		$source_disk = $this->argument('source_disk');
+		$destination_disk = $this->argument('destination_disk');
+
+		echo "Copying files from disk - $source_disk to disk - $destination_disk.\n";
+		$files = Storage::disk($source_disk)->allFiles('smartarchive_assets');
+		foreach ($files as $file) {
+			try{
+				Storage::disk($destination_disk)->put($file, Storage::disk($source_disk)->get($file));
+				echo "Copied ".$file."\n";
+			}
+			catch(\Exception $e){
+				echo "ERROR: ".$e->getMessage()."\n";
+			}
+		}
+    }
+}
