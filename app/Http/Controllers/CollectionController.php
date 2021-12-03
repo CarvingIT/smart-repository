b9f2d5f4ac5a350ae456a461536b1ceb64206671
Ketan Kulkarni <ketan@carvingit.com>
2021-12-03 09:53:45 +0530

    interim update; more work needed

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -189,8 +189,26 @@ class CollectionController extends Controller
 	}
 
     public function getMetaFilteredDocuments($request, $documents){
-        $all_meta_filters = Session::get('meta_filters');
-        $meta_filters = empty($all_meta_filters[$request->collection_id])?null:$all_meta_filters[$request->collection_id];
+		// check if meta filters are present in the query
+		$query_params = $request->query();
+		$meta_filters_query = array();
+		foreach($query_params as $p=>$v){
+			if(preg_match('/^meta_(\d*)/', $p, $matches)){
+				// currently, no support for operator in the query string parameters
+				// default operator is '='
+				$meta_filters_query[] = array('field_id'=>$matches[1], 'operator'=>'=', 'value'=>$v);
+			}
+		}
+		if(count($meta_filters_query)>0){
+			$meta_filters = $meta_filters_query;
+			print_r($meta_filters);exit;
+			//$meta_filters = [];
+		}
+		else{
+			// else take from the session
+        	$all_meta_filters = Session::get('meta_filters');
+        	$meta_filters = empty($all_meta_filters[$request->collection_id])?null:$all_meta_filters[$request->collection_id];
+		}
         foreach($meta_filters as $mf){
             if($mf['operator'] == '='){
                 $documents->whereHas('meta', function (Builder $query) use($mf){
@@ -312,10 +330,10 @@ class CollectionController extends Controller
             $documents = $this->getTitleFilteredDocuments($request, $documents);
 		}
         // get Meta filtered documents
-        $all_meta_filters = Session::get('meta_filters');
-        if(!empty($all_meta_filters[$request->collection_id])){
+        //$all_meta_filters = Session::get('meta_filters');
+        //if(!empty($all_meta_filters[$request->collection_id])){
             $documents = $this->getMetaFilteredDocuments($request, $documents);
-        }
+        //}
 
 	// get approval exception 
 	// the exceptions will be removed from the models with ->whereNotIn 
@@ -400,10 +418,10 @@ class CollectionController extends Controller
             $documents = $this->getTitleFilteredDocuments($request, $documents);
 		}
         // get Meta filtered documents
-        $all_meta_filters = Session::get('meta_filters');
-        if(!empty($all_meta_filters[$request->collection_id])){
+        //$all_meta_filters = Session::get('meta_filters');
+        //if(!empty($all_meta_filters[$request->collection_id])){
             $documents = $this->getMetaFilteredDocuments($request, $documents);
-        }
+        //}
 
         // content search
         if(!empty($request->search['value']) && strlen($request->search['value'])>3){
