463fcabadd16556f2032ded998963b55b2b14e46
Ketan Kulkarni <ketan@carvingit.com>
2022-12-06 07:28:14 +0530

    local installation of tinymce; removed the error related to domain not registered on tinycloud

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app',['class'=> 'off-canvas-sidebar'])
 @push('js')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-<script src="https://cdn.tiny.cloud/1/57yud8hji4ltgdkaea05vb4gx1yvvbqdmzx605fgpsauwm10/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<link href="/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<script src="/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="/js/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
 <script>
 tinymce.init({
-                                selector: '#document_description',
+                                //selector: '#document_description',
+                                selector: 'textarea',
                                 plugins: [
                                     "advlist autolink lists link image charmap print preview hr anchor pagebreak",
                                     "searchreplace wordcount visualblocks visualchars code fullscreen",
