d45f39e400ea3ea2a0ba8591dadd910046b198f6
root <root@Ubuntu.myguest.virtualbox.org>
2023-09-12 17:40:09 +0530

    Change the isa layouts welcome view.

---
+++ resources/views/isa/welcome.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app',['class' => 'off-canvas-sidebar','title'=>'Smart Repository','activePage'=>'contact','titlePage'=>'Contact Us'])
+
+@section('content')
+@php
+	$conf = \App\Sysconfig::all();
+	$settings = array();
+	foreach($conf as $c){
+		$settings[$c->param] = $c->value;
+	}
+@endphp
+<div class="container">
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header card-header-primary"><h4 class="card-title">Contact Us</h4></div>
+
+                <div class="card-body">
+                @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
+
