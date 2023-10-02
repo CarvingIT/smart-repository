3075d0c0b52c60f8b49cea2cbf23b57aec3716e5
Ketan Kulkarni <ketan@carvingit.com>
2023-10-02 22:52:42 +0530

    blog related event and listener

--- app/BinshopsPost.php
+++ app/BinshopsPost.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class BinshopsPost extends Model
 {
-    //
-	protected $table = 'binshops_posts';
-
 	public function approvals(){
 		return $this->morphMany('App\Approval', 'approvable');
 	}
--- app/Listeners/BinshopsPostSaved.php
+++ app/Listeners/BinshopsPostSaved.php
@@ -27,13 +27,16 @@ class BinshopsPostSaved
      */
     public function handle($event)
     {
-		if($event->binshops_post->approvals->count() == 0){
+		//if($event->binshops_post->approvals->count() == 0){
 			// get the first role id from approval workflow
 			$collection = Collection::find(1);
 			$col_conf = json_decode($collection->column_config);
 			$approvers = $col_conf->approved_by;
-			$approval_record = new Approval(['approved_by_role'=>$approvers[0]]);
-			$event->binshops_post->approvals()->save($approval_record);	
-		}
+			$approval_record = new Approval();
+			$approval_record->approved_by_role=$approvers[0];
+			$approval_record->approvable_id=$event->binshopsBlogPost->id;
+			$approval_record->approvable_type='App\BinshopPost';
+			$approval_record->save();
+		//}
     }
 }
