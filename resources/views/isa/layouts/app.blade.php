9f95979e96430481ecfa6c29a8081575dded1224
root <root@Ubuntu.myguest.virtualbox.org>
2023-10-04 15:48:49 +0530

    Update Footer link.

--- resources/views/isa/layouts/app.blade.php
+++ resources/views/isa/layouts/app.blade.php
@@ -19,6 +19,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
   <!-- Vendor CSS Files -->
+  <link rel="stylesheet" href="/css/isa/material/css/material-dashboard.css">
   <link href="/css/isa/material/css/material-dashboard.css" rel="stylesheet">
   <link href="/css/isa/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="/css/isa/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
