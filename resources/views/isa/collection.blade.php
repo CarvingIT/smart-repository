947f3d8f7a95785caa84dc051f041be99ef47e8b
Ketan Kulkarni <ketan@carvingit.com>
2023-10-07 19:23:59 +0530

    If meta field - abstract has any value that is shown in the search results instead of initial 250 chars of text_content

--- resources/views/isa/collection.blade.php
+++ resources/views/isa/collection.blade.php
@@ -145,10 +145,21 @@ foreach($tags as $t){
 	@foreach($results as $result)
 		@php 
 			$document = \App\Document::find($result->id);
+			$meta_fields = $document->collection->meta_fields;
+			$abstract_field_id = null;
+			foreach($meta_fields as $m){
+				if(strtolower($m->label) == 'abstract'){
+					$abstract_field_id = $m->id;
+					break;
+				}
+			}
 		@endphp
 <p><b><a href="/collection/{{ $collection->id }}/document/{{ $result->id }}"><i class="fa fa-file-text" aria-hidden="true"></i>&nbsp; {{ $result->title }}</a></b><br>
-		{{-- $result->text_content --}}
+		@if (!empty($abstract_field_id) && !empty($document->meta_value($abstract_field_id)))
+		{{ $document->meta_value($abstract_field_id) }}
+		@else
 		{{ \Illuminate\Support\Str::limit($document->text_content, 250, $end='...') }}
+		@endif
 		</p>
 	@endforeach
 	@else
