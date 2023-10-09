4817f8633ab731785433edcfb9e1c15a9a447538
root <root@Ubuntu.myguest.virtualbox.org>
2023-10-09 15:17:08 +0530

    Update Search box in home.

--- resources/views/isa/welcome.blade.php
+++ resources/views/isa/welcome.blade.php
@@ -11,9 +11,9 @@
           <h4 class="text-center" data-aos="fade-up">Regulation Resource Repository for Solar Energy</h4>
           <p class="text-center" data-aos="fade-up" data-aos-delay="100">A comprehensive data repository for all regulations in ISA member countries relating to Solar Energy</p>
 
-          <form action="#" class="form-search d-flex align-items-stretch mb-4" data-aos="fade-up" data-aos-delay="200">
+          <form action="/documents/isa_document_search" class="form-search d-flex align-items-stretch mb-4" data-aos="fade-up" data-aos-delay="200" method="get" id="isa_search" name="isa_search">
             <input type="text" class="form-control" placeholder="What are you looking for?">
-            <button type="submit" class="btn btn-primary">Search</button>
+            <button type="submit" value="Search" name="isa_search" class="btn btn-primary">Search</button>
           </form>
 
           <div class="row gy-4 mb-3" data-aos="fade-up" data-aos-delay="400">
