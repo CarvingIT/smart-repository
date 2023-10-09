411116ddff6760637bdaa9816d3a661b0d025f3f
root <root@Ubuntu.myguest.virtualbox.org>
2023-10-09 18:51:37 +0530

    Update for home search.

--- resources/views/isa/welcome.blade.php
+++ resources/views/isa/welcome.blade.php
@@ -12,7 +12,7 @@
           <p class="text-center" data-aos="fade-up" data-aos-delay="100">A comprehensive data repository for all regulations in ISA member countries relating to Solar Energy</p>
 
           <form action="/documents/isa_document_search" class="form-search d-flex align-items-stretch mb-4" data-aos="fade-up" data-aos-delay="200" method="get" id="isa_search" name="isa_search">
-            <input type="text" class="form-control" placeholder="What are you looking for?">
+            <input type="text" class="form-control form-group" placeholder="What are you looking for abc?">
             <button type="submit" value="Search" name="isa_search" class="btn btn-primary">Search</button>
           </form>
 
