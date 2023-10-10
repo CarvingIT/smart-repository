47b8a3b83716dd642c882781d98fe6af9c3d0b9a
root <root@Ubuntu.myguest.virtualbox.org>
2023-10-10 19:02:32 +0530

    Update the theme page.

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -7,9 +7,8 @@ use App\Taxonomy;
 
 class ThemeController extends Controller
 {
-     public function index(Taxonomy $taxonomy)
+    public function index(Taxonomy $taxonomy)
     {
-  
 	return view('isa.themes', ['taxonomies'=>$taxonomy->all(), 'activePage'=>'taxonomies-management', 'titlePage' => 'Taxonomies']);
     }
 }
