3ad613910ac7e0801bd811783251f4fd49d2d21e
Shraddha Kulkarni <shraddha@carvingit.com>
2023-10-13 12:28:21 +0530

    Search hid for blog index page

--- resources/views/vendor/binshopsblog/sitewide/search_form.blade.php
+++ resources/views/vendor/binshopsblog/sitewide/search_form.blade.php
@@ -1,3 +1,4 @@
+<!--
 <div style='max-width:500px;margin:50px auto;' class='search-form-outer'>
     <form method='get' action='{{route("binshopsblog.search", app('request')->get('locale'))}}' class='text-center'>
         <h4>Search for something in our blog:</h4>
@@ -5,3 +6,4 @@
         <input type='submit' value='Search' class='btn btn-primary m-2'>
     </form>
 </div>
+-->
