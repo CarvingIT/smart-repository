995e83d6220e45df0f54b20868000b0e2241feee
root <root@Ubuntu.myguest.virtualbox.org>
2023-10-27 21:14:59 +0530

    Upadate Contact page.

--- resources/views/isa/contact-us.blade.php
+++ resources/views/isa/contact-us.blade.php
@@ -17,15 +17,17 @@
 
                 <div class="card-body">
                     <div>
-						<p>
+						<p> 
 						<strong>International Solar Alliance Secretariat</strong><br/>
-						Surya Bhawan,<br/>
-						National Institute of Solar Energy Campus Gwal Pahari,<br/>
-						Faridabad-Gurugram Road,<br/>
-						Gurugram, Haryana – 122003, India<br/>
-						Tel: +91 124 362 3090/69<br/>
-						Email: info@isolaralliance.org
-						</p>	
+						<span class="bi bi-geo-alt-fill c-icon"></span><span class="ct-geo">Surya Bhawan,</span><br/>
+						<span class="ct-sub">National Institute of Solar Energy Campus Gwal Pahari,</span><br/>
+						<span class="ct-sub">Faridabad-Gurugram Road,</span><br/>
+						<span class="ct-sub">Gurugram, Haryana – 122003, India</span><br/>
+						<span class="bi bi-telephone c-icon"></span> +91 124 362 3090/69<br/>
+                        <span class="bi bi-envelope c-icon"></span> info@isolaralliance.org
+						</p>
+                        </a>
+          	
                     </div>
                 </div>
             </div>
