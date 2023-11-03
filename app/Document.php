c73edb16ae9274393fba89828b3032a832dbca71
Ketan Kulkarni <ketan@carvingit.com>
2023-11-03 07:21:49 +0530

    meta_value updated to return just "all" if that is one of the values

--- app/Document.php
+++ app/Document.php
@@ -92,6 +92,7 @@ class Document extends Model implements Auditable
 					$taxonomy_models = Taxonomy::whereIn('id', @json_decode($meta_value->value))->get();
 					$terms = [];
 					foreach($taxonomy_models as $t){
+						if(strtolower($t->label) == 'all') return $t->label; // special value (ALL)
 						$terms[] = $t->label;
 					}
 					return implode(', ',$terms);
