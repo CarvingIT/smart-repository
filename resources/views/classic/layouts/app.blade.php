cac8bc6127d527eb04fc9f1c77bad5b11a7ce914
Ketan Kulkarni <ketan@carvingit.com>
2023-11-20 22:04:56 +0530

    updated browser tab title

--- resources/views/classic/layouts/app.blade.php
+++ resources/views/classic/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>ISA - international solar alliance</title>
+  <title>{{ env('APP_NAME') }}</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
   <!-- Favicons -->
