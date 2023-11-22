8235b084cadf0845c71dd3adc58a3e4c12f628e6
Ketan Kulkarni <ketan@carvingit.com>
2023-11-22 19:04:17 +0530

    access to blog admin panel restricted

--- app/User.php
+++ app/User.php
@@ -164,22 +164,10 @@ class User extends Authenticatable implements MustVerifyEmail
         // Enter the logic needed for your app.
         // Maybe you can just hardcode in a user id that you
         //   know is always an admin ID?
-
-        if (       $this->id === 1
-             && $this->email === "your_admin_user@your_site.com"
-           ){
-
-           // return true so this user CAN edit/post/delete
-           // blog posts (and post any HTML/JS)
-
-           return true;
-        }
-
-        // otherwise return false, so they have no access
-        // to the admin panel (but can still view posts)
-
-        //return false;
-        return true;
+		if($this->hasRole('admin')){
+			return true;
+		}
+        return false;
     }
 
 ///// 
