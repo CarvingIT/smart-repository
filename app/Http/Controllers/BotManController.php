23a2e108012cf3a2adbe3837afc5fc2ccf117cd5
Ketan Kulkarni <ketan@carvingit.com>
2023-12-01 21:30:25 +0530

    regex for utf-8 fix

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -149,6 +149,19 @@ class BotManController extends Controller
 					$info_from_doc .= $meta_info;
 					//convert to utf-8
 					//$info_from_doc = iconv(mb_detect_encoding($info_from_doc, mb_detect_order(), true), "UTF-8", $info_from_doc);
+					$regex = <<<'END'
+/
+  (
+    (?: [\x00-\x7F]                 # single-byte sequences   0xxxxxxx
+    |   [\xC0-\xDF][\x80-\xBF]      # double-byte sequences   110xxxxx 10xxxxxx
+    |   [\xE0-\xEF][\x80-\xBF]{2}   # triple-byte sequences   1110xxxx 10xxxxxx * 2
+    |   [\xF0-\xF7][\x80-\xBF]{3}   # quadruple-byte sequence 11110xxx 10xxxxxx * 3 
+    ){1,100}                        # ...one or more times
+  )
+| .                                 # anything else
+/x
+END;
+$info_from_doc = preg_replace($regex, '$1', $info_from_doc);
 
 					//$chunks_doc = Util::createTextChunks($info_from_doc, 4000, 1000);
 					$chunks_doc = Util::createTextChunks($info_from_doc, 4000, 200);
