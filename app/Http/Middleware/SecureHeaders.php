f538a0fd8a3b6276e086d1f1bb0fd89138b2d019
Ketan Kulkarni <ketan@carvingit.com>
2023-12-18 15:33:21 +0530

    removed a security header X-frame deny in order for the filemanager to work

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -25,10 +25,10 @@ class SecureHeaders
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
-        $response->headers->set('X-Frame-Options', 'DENY');
+        //$response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('Clear-Site-Data', "'cache', 'cookies', 'storage', 'executionContents'");
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-        $response->headers->set('Content-Security-Policy', "script-src 'self' 'unsafe-inline' code.jquery.com mozilla.github.io; style-src 'self' 'unsafe-inline' fonts.googleapis.com code.jquery.com"); 
+        $response->headers->set('Content-Security-Policy', "script-src 'self' 'unsafe-inline' cdn.jsdelivr.net code.jquery.com mozilla.github.io; style-src 'self' 'unsafe-inline' cdn.jsdelivr.net	fonts.googleapis.com code.jquery.com"); 
 		foreach($this->unwantedHeaderList as $h){
 			$response->headers->remove($h);
 		}
