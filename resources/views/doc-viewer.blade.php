a2f7374c7280c6e09ec61b1052a185292d65d0a0
Ketan Kulkarni <ketan@carvingit.com>
2025-07-29 10:27:28 +0530

    2 column layout for the document viewer

--- resources/views/doc-viewer.blade.php
+++ resources/views/doc-viewer.blade.php
@@ -2,28 +2,53 @@
 <head>
 <title>{{ env('APP_NAME', 'Smart Repository') }}::Document Viewer</title>
 <link rel="icon" type="image/png" href="/material/img/favicon.png">
+<style>
+.row {
+  display:flex;
+  height:100%;
+  overflow:clip;
+}
+#meta-view {
+padding-left:1em;
+flex:40%;
+}
+#doc-view {
+  flex:60%;
+}
+body{
+margin:0;
+}
+</style>
 </head>
 <body>
 @php
-if(!is_null($path_count)){
     $doc = \App\Document::find($document_id);
+@endphp
+<div class="row">
+<div id="meta-view">
+	<h4>Associated Information</h4>
+	@foreach($doc->meta as $m)
+	<p><strong>{{ $m->meta_field->label }}</strong><br />{!! $m->value !!}</p>
+	@endforeach
+</div>
+<div id="doc-view">
+@php
+if(!is_null($path_count)){
     $path = json_decode($doc->path);
     $file_url = $path[$path_count];
     $file_path  = $path[$path_count];
 @endphp
-<iframe id="pdfreader" class="pdf" src="/js/ViewerJS/#../../collection/{{ $collection_id }}/document/{{ $document_id }}/details/{{ $path_count }}" width="100%" height="100%"></iframe> 
+<iframe id="pdfreAder" class="pdf" src="/js/ViewerJS/?title={{ $doc->title }}#../../collection/{{ $collection_id }}/document/{{ $document_id }}/details/{{ $path_count }}" width="100%" height="100%"></iframe> 
 @php
 }
 else{
 @endphp
-<iframe id="pdfreader" class="pdf" src="/js/ViewerJS/#../../collection/{{ $collection_id }}/document/{{ $document_id }}" width="100%" height="100%"></iframe>
+<iframe id="pdfreader" class="pdf" src="/js/ViewerJS/?title={{ $doc->title }}#../../collection/{{ $collection_id }}/document/{{ $document_id }}" width="100%" height="100%"></iframe>
 @php
 }
 @endphp
-<!--iframe id="pdfreader" class="pdf" src="/js/ViewerJS/#../../collection/{{ $collection_id }}/document/{{ $document_id }}" 
-            width="100%" height="100%">
-</iframe-->
-
+</div>
+</div>
 <script>
 	var iframe = document.getElementById('pdfreader');
 
