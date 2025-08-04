ee3eea598670937995c042a9341421435b928bf7
Ketan Kulkarni <ketan@carvingit.com>
2025-08-04 20:31:53 +0530

    bugfix for persistence of the search term

--- resources/views/collection.blade.php
+++ resources/views/collection.blade.php
@@ -314,8 +314,13 @@ function randomString(length) {
 			<div class="float-container" style="width:100%;">
             @php
                 $old_search_query = !empty(app('request')->input('search_term'))? app('request')->input('search_term') : ''; 
+                // set a new search query
+                if(!empty(app('request')->input('search_term'))){
+                    Session::put('search_query', $old_search_query);
+                }
                 $session_search_query = Session::get('search_query');
-                $referer = request()->headers->get('referer');
+                $parsed_referer = parse_url(request()->headers->get('referer'));
+                $referer = $parsed_referer['scheme'] . '://' . $parsed_referer['host'] . (isset($parsed_referer['port']) ? ':' . $parsed_referer['port'] : '') . $parsed_referer['path'];
                 if($referer && $referer === Request::url() 
                     && empty($old_search_query) && !empty($session_search_query)){
                     $old_search_query = $session_search_query;
