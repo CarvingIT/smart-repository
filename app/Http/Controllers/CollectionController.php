9f10ab3807f4a3748ea3b8490e6d2727f0b73562
Ketan Kulkarni <ketan@carvingit.com>
2025-08-21 08:34:43 +0530

    minor bug fixes

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -403,6 +403,7 @@ $j++;
 			$extra_attributes['numeric_min_value'] = $request->input('numeric_min_value');
 			$extra_attributes['numeric_max_value'] = $request->input('numeric_max_value');
 			$extra_attributes['show_on_details_page'] = $request->input('show_on_details_page');
+			$extra_attributes['show_parents'] = $request->input('show_parents');
 			$extra_attributes['results_classname'] = $request->input('results_classname');
 			$extra_attributes['filter_width_on_collection_page'] = $request->input('filter_width_on_collection_page');
 			$meta_field->extra_attributes = json_encode($extra_attributes);
