70892391dd2662c917949f806a2e56e302e941dd
Ketan Kulkarni <ketan@carvingit.com>
2025-09-16 15:57:51 +0530

    bugfix

--- app/Console/Commands/UpdateMeta.php
+++ app/Console/Commands/UpdateMeta.php
@@ -83,7 +83,7 @@ class UpdateMeta extends Command
 					$taxo_parent = $meta_field->options;
 					// there can be more than one values separated by pipes "|"
 					$val_ar = explode("|", $values[$i]);
-					$t_models = Taxonomy::where('parent', $taxo_parent)
+					$t_models = Taxonomy::where('parent_id', $taxo_parent)
 						->whereIn('label', $val_ar)->get();
 					$t_ids = [];
 					foreach($t_models as $t){
