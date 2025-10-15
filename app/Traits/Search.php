22ff52f55f4a2750a2278feec062b967b9abe6b4
Ketan Kulkarni <ketan@carvingit.com>
2025-10-15 22:49:19 +0530

    minor update

--- app/Traits/Search.php
+++ app/Traits/Search.php
@@ -428,6 +428,7 @@ trait Search{
             $document_ids = [];
 		    try{
                 Log::debug(json_encode($params));
+                $count_response = $client->count($params);
            	    $response = $client->search($params);
                 foreach($response['hits']['hits'] as $h){
                     $document_ids[] = $h['_id'];
@@ -446,10 +447,10 @@ trait Search{
         $columns = array('type', 'title', 'size', 'updated_at');
     	//if(isset($document_ids) && count($document_ids) > 0){
         //$filtered_count = $documents->count();
-        $filtered_count = $total_count;// to be updated
+        $filtered_count = $count_response['count'];// to be updated
 	    if(isset($document_ids)){
 	        Log::debug('Found: '.@count($document_ids));
-            Log::debug('Listed IDs: '.json_encode($document_ids));        
+            //Log::debug('Listed IDs: '.json_encode($document_ids));        
        	    $documents = \App\Document::whereIn('id', $document_ids);
             if(!empty($search_term)) $filtered_count = $documents->count();
 	    }
