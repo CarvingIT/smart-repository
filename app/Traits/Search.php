e8b7cc4102a40a0f6b18f72c37b0b119379f6ff2
Ketan Kulkarni <ketan@carvingit.com>
2025-10-17 19:34:36 +0530

    pagination related

--- app/Traits/Search.php
+++ app/Traits/Search.php
@@ -454,7 +454,7 @@ trait Search{
 
         $columns = array('type', 'title', 'size', 'updated_at');
     	//if(isset($document_ids) && count($document_ids) > 0){
-        $filtered_count = $count_response['count'];// to be updated
+        $filtered_count = ($count_response['count'] > 10000)?10000:$count_response['count'];// to be updated
 
 	    if(isset($document_ids)){
 	        Log::debug('Found: '.@count($document_ids));
