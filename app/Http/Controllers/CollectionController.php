006215c329b38eece2077377de4af4b25749637e
avishkar bhosale <avishkarbhosale519@gmail.com>
2025-11-09 18:45:59 +0530

    Add collection sorting feature with gear icon dropdown.

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -50,7 +50,7 @@ class CollectionController extends Controller
     }
 
 
-    public function list(){
+    public function list(Request $request){
 		$collections = $this->userCollections(['VIEW_OWN','VIEW','MAINTAINER']);
         $stats_rows = DB::table('documents')
             ->select('collection_id', DB::raw('count(*) as cnt'), DB::raw('sum(size) as size'))
@@ -61,7 +61,32 @@ class CollectionController extends Controller
         foreach($stats_rows as $stat){
             $stats[$stat->collection_id] = $stat;
         }
-        return view('collections', ['title'=>'Smart Repository','activePage'=>'collections','titlePage'=>'Collections','collections'=>$collections, 'stats'=>$stats]);
+        
+        // Handle sorting
+        $sort_by = $request->input('sort_by', 'name_asc'); // default to alphabetical ascending
+        
+        switch($sort_by) {
+            case 'name_asc':
+                $collections = $collections->sortBy('name', SORT_NATURAL|SORT_FLAG_CASE);
+                break;
+            case 'name_desc':
+                $collections = $collections->sortByDesc('name', SORT_NATURAL|SORT_FLAG_CASE);
+                break;
+            case 'size_asc':
+                $collections = $collections->sortBy(function($collection) use ($stats) {
+                    return isset($stats[$collection->id]) ? (int)$stats[$collection->id]->cnt : 0;
+                });
+                break;
+            case 'size_desc':
+                $collections = $collections->sortByDesc(function($collection) use ($stats) {
+                    return isset($stats[$collection->id]) ? (int)$stats[$collection->id]->cnt : 0;
+                });
+                break;
+            default:
+                $collections = $collections->sortBy('name', SORT_NATURAL|SORT_FLAG_CASE);
+        }
+        
+        return view('collections', ['title'=>'Smart Repository','activePage'=>'collections','titlePage'=>'Collections','collections'=>$collections, 'stats'=>$stats, 'sort_by'=>$sort_by]);
     }
 
     public function save(Request $request){
