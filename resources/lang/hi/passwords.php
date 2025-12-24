28486ead34366c42c81d9ba12482e6d70f80389c
prasenbhandari <prasenbhandari2003@gmail.com>
2025-12-24 19:06:00 +0530

    added hindi localization

---
+++ resources/lang/hi/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'आपका पासवर्ड रिसेट हो गया है!',
+    'sent' => 'हमने आपको पासवर्ड रिसेट लिंक ईमेल कर दिया है!',
+    'throttled' => 'पुनः प्रयास करने से पहले कृपया प्रतीक्षा करें।',
+    'token' => 'यह पासवर्ड रिसेट टोकन अमान्य है।',
+    'user' => "हमें उस ईमेल पते वाला कोई उपयोगकर्ता नहीं मिला।",
+
+];
