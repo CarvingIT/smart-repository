bbc4ecc7c2c15aa768c27af8ee0e009c0a4e9c9b
princethakur931 <princethakur545454@gmail.com>
2026-05-21 21:03:13 +0530

    fix dropbox issue

--- resources/views/document_approval.blade.php
+++ resources/views/document_approval.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app',['class'=> 'off-canvas-sidebar'])
 @push('js')
-<link href="/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-<script src="/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+{{-- Bootstrap 4 is already loaded by layout, do NOT load Bootstrap 5 here (causes dropdown conflict) --}}
 
 <link rel="stylesheet" href="/build/assets/css/dataTables.dataTables.min.css" />
 <link rel="stylesheet" href="/build/assets/css/jquery-ui.min.css" />
